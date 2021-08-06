6589ae0c3f0ce31651f01e3da8b29b6ba1d884ee
josegcmoraes <josegcmoraes@gmail.com>
2021-08-06 08:55:53 -0300

    02-dtmoney

---
+++ 02-dtmoney/src/App.tsx
@@ -0,0 +1,19 @@
+// import './App.css';
+import { GlobalStyle } from "./styles/global";
+// import styled from 'styled-components';
+
+// const Title = styled.h1`
+//   color: #8257e6;
+//   font-size: 64px;
+// `
+
+export function App() {
+  return (
+    <div className="App">
+      {/* <Title >Hey td bem</Title> */}
+      <GlobalStyle />
+      <h1>hi all ... font em nm</h1>
+    </div>
+  );
+}
+
